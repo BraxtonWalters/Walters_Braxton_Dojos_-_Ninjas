658aaa77edb51315d873addc47d937e216fc3967
Braxton Walters <braxton.walters@gmail.com>
2023-07-13 01:16:25 -0400

    init commit

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+# import controllers!!!!!!!!!!!fucking look at me!!!!@!@!@!@!@121212132e@!#!$!##!@!!!!!!!!!!!!!!!!!!!! 
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
